62e98aedb6b6e6ebf510c84fd0338be4856e6698
Anton Dementev <antdementev@gmail.com>
2018-03-01 09:59:29 +0300

    01.03.2018

--- homework3/src/main/java/ru.otus.homework3/Main.java
+++ homework3/src/main/java/ru.otus.homework3/Main.java
@@ -1,7 +1,10 @@
 package ru.otus.homework3;
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Антон Дементьев on 18.02.2018.
@@ -15,29 +18,45 @@ public class Main {
         List<Integer> list1 = new ArrayList<>();
 
         Random random = new Random();
+
         for (int i = 0; i < 20; i++) {
             list.add(i);
             list1.add(random.nextInt(10) + 1);
         }
-        System.out.println("list.size(): " + list.size());
+        System.out.println("Size: " + list.size());
 
+        System.out.println("Get:");
         for (int i = 0; i < 20; i++)
             System.out.print(list.get(i) + " ");
-
-        Collections.copy(list, list1);
-
         System.out.println();
+
+        System.out.println("Copy:");
+        Collections.copy(list, list1);
         for (Integer integer : list)
             System.out.print(integer + " ");
-        System.out.println();
+
+
+        System.out.println("\nSort:");
         Collections.sort(list);
         for (Integer integer : list)
             System.out.print(integer + " ");
-        System.out.println("\nУдаление");
+
+        System.out.println("\nSize: " + list.size());
+
+        System.out.println("AddAll:");
         list.addAll(list1);
+        for (Integer integer : list)
+            System.out.print(integer + " ");
+
+        System.out.println("\nSize: " + list.size());
+
+        System.out.println("Remove:");
+
+
         for (int i = 39; i >= 0; i--)
             System.out.print(list.remove(i) + " ");
-        System.out.println("\nlist.size(): " + list.size());
+
+        System.out.println("\nSize: " + list.size());
 
     }
 
